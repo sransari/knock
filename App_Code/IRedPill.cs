5d8ceff2d069934bdb2402c73ddad3a3938cd2f4
Salman Al-Ansari <salman.rahmat@gmail.com>
2015-02-20 18:30:26 +0300

    RedPill service finalized

---
+++ App_Code/IRedPill.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace knockknock.readify.net
+{
+    [ServiceContract (Namespace = "http://KnockKnock.readify.net")]
+    public interface IRedPill
+    {
+
+        [OperationContract]
+        Guid WhatIsYourToken();
+
+        [OperationContract]
+        [FaultContract(typeof(System.ArgumentOutOfRangeException))]
+        long FibonacciNumber(long n);
+
+        [OperationContract]
+        TriangleType WhatShapeIsThis(int a, int b, int c);
+
+        [OperationContract]
+        [FaultContract(typeof(System.ArgumentNullException))]
+        string ReverseWords(string s);
+    }
+
+    [DataContract (Namespace="http://KnockKnock.readify.net")]
+    public enum TriangleType : int
+    {
+
+        [EnumMember]
+        Error = 0,
+
+        [EnumMember]
+        Equilateral = 1,
+
+        [EnumMember]
+        Isosceles = 2,
+
+        [EnumMember]
+        Scalene = 3,
+    }
+}
---
+++ App_Code/RedPill.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace knockknock.readify.net
+{
+   [ServiceBehavior(Namespace="http://KnockKnock.readify.net")]
+    public class RedPill : IRedPill
+    {
+        public Guid WhatIsYourToken()
+        {
+            return new Guid("2f9b896f-febe-48ad-91bb-5fdfe23397fa");
+        }
+
+        public long FibonacciNumber(long n)
+        {
+            long a = 0;
+            long b = 1;
+
+            long tempN = Math.Abs(n);
+
+            if  (tempN > 92)
+            {
+                throw new ArgumentOutOfRangeException("Parameter name: n", "Fib(>92) will cause a 64-bit integer overflow");
+            }
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < tempN; i++)
+            {
+                long temp = a;
+                a = b;
+                b = temp + b;
+            }
+            if (n < 0)
+            {
+                return -1 * a;
+            }
+            else
+            {
+                return a;
+            }
+        }
+
+        public string ReverseWords(string s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException("There is no word to reverse");
+            }
+            else
+            {
+                char[] charArray = s.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            }
+        }
+
+        public TriangleType WhatShapeIsThis(int a, int b, int c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0
+                || (a + b <= c) || (a + c <= b) || (b + c <= a))
+            {
+                return TriangleType.Error;
+            }
+            else if ((a == b) && (b == c))
+            {
+                return TriangleType.Equilateral;
+            }
+            else if ((a == b) || (b == c) || (a == c))
+            {
+                return TriangleType.Isosceles;
+            }
+            else if ((a != b) && (b != c) && (a != c))
+            {
+                return TriangleType.Scalene;
+            }
+            else return TriangleType.Error;
+        }
+    }
+}
